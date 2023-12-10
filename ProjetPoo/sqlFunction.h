7a3fa63747c31df140d9e2261aabde975df1ce63
Vore667 / Arthur SANGLIER <rox.gaming67@gmail.com>
2023-12-10 18:32:36 +0100

    Implementation supprimer
    Implémentation supprimer

--- ProjetPoo/sqlFunction.h
+++ ProjetPoo/sqlFunction.h
@@ -17,7 +17,8 @@ namespace NS_sql_Function{
 		System::Data::DataSet^ selectSpe(System::String^,System::String^,List<System::Windows::Forms::TextBox^>^ nonEmptyTxtBox, System::String^ elem);
 		void insertSpe(System::String^ res, System::String^ tableName, NS_objFonction::objFonction^ objFonction, List<System::Windows::Forms::TextBox^>^ TxtBoxList);
 		System::Windows::Forms::TextBox^ getSpeID(String^ tableName, List<System::Windows::Forms::TextBox^>^ TxtBoxList, String^ AN, String^ id);
-
+		System::Data::DataSet^ updateSpe(System::String^ dataTableName, System::String^ tableName, List<System::Windows::Forms::TextBox^>^ TxtBoxList, System::String^ AN, System::String^ id);
+		System::Data::DataSet^ deleteSpe(System::String^ dataTableName, System::String^ tableName, List<System::Windows::Forms::TextBox^>^ TxtBoxList, System::String^ AN, System::String^ id);
 	};
 }
 
